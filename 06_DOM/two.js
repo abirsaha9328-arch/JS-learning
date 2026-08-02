cf809ba314a88449d878118a8bf0023ff81d6d53
abirsaha9328-arch <abirsaha9328@gmail.com>
2026-08-02 18:25:28 +0000

    new element in DOM

---
+++ 06_DOM/two.js
@@ -0,0 +1,17 @@
+const targe1 = document.querySelector(".parent")
+console.log(targe1)
+console.log(targe1.children)
+// console.log(targe1.children[0].textContent)
+
+// without create a mew array print the   value
+
+// const targe2 = Array.from(targe1.children)
+// targe2.forEach( (item) => {
+//     console.log(item.textContent)
+// })
+
+for(let i = 0 ; i < targe1.children.length ; i++)
+{
+    targe1.children[i].style.color = '#fff'
+    console.log(targe1.children[i].textContent)
+}
