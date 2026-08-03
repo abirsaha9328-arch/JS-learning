a284ea1d1fad622c1a4ee090116188b037c057cd
abirsaha9328-arch <abirsaha9328@gmail.com>
2026-08-03 14:29:50 +0000

    Project 1 done

--- 07_Project/Project1/script.js
+++ 07_Project/Project1/script.js
@@ -1,5 +1,60 @@
-let Box = document.querySelector(".colors")
+// let grayBox = document.querySelector(".gray")
+// let whiteBox = document.querySelector(".white")
+// let blueBox = document.querySelector(".blue")
+// let yellowBox = document.querySelector(".yellow")
+// grayBox.addEventListener('click' , () =>{
+//     document.body.style.backgroundColor = "gray"
+// })
 
-Box.addEventListener('click' , () =>{
-    console.log("Hello Box")
+// whiteBox.addEventListener('click' , () =>{
+//     document.body.style.backgroundColor = "white"
+// })
+
+// blueBox.addEventListener('click' , () =>{
+//     document.body.style.backgroundColor = "blue"
+// })
+
+// yellowBox.addEventListener('click' , () =>{
+//     document.body.style.backgroundColor = "yellow"
+// })
+
+
+
+// Optimal solution
+
+// This is my code 
+
+const parentBox = document.querySelectorAll(".box")
+const setColor = document.body
+
+parentBox.forEach( (item)=>{
+    
+    item.addEventListener("click" , (event)=>{
+        
+       const colorID = event.target.id
+
+       if(colorID === "gray")
+       {
+            setColor.style.background = event.target.id
+       }
+
+       if(colorID === "white")
+       {
+            setColor.style.background = event.target.id
+       }
+
+       if(colorID === "blue")
+       {
+            setColor.style.background = event.target.id
+       }
+
+       else
+       {
+            setColor.style.background = event.target.id
+       }
+
+    })
+
 })
+
+
