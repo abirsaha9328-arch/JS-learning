310fae7c32a7ea497d738f96ff487d492d323c5c
abirsaha9328-arch <abirsaha9328@gmail.com>
2026-08-04 16:38:54 +0000

    Project 2 done

--- 07_Project/Project4/script.js
+++ 07_Project/Project4/script.js
@@ -1,16 +1,35 @@
 const prevGuesses = document.querySelector(".guesses");
 const remGuesses = document.querySelector(".lastResult");
 const submitButton = document.querySelector("#subt");
-const inputValue = document.querySelector("#guessField").value
 const form = document.querySelector(".form");
-
-
-
+let guessCount = 0;
+let totalGuesses = 10;
 form.addEventListener("submit", (event) => {
     event.preventDefault();
+        let randomNumber = Math.floor(Math.random() * 100) + 1;
+        const inputValue =  Number(document.querySelector("#guessField").value)
+        console.log(`Random value : ${randomNumber}`)
+        console.log(`Input Value : ${inputValue}`)
+        guessCount++;
+        prevGuesses.textContent = `${inputValue}`
+        remGuesses.textContent = `${totalGuesses - guessCount}`
+        console.log(`count : ${guessCount}`)
 
-    var a = 12
+        if(randomNumber === inputValue)
+        {
+            console.log('Value matching')
+        }
+        
+       
+
+        if(guessCount === 10 && inputValue != randomNumber)
+        {
+            console.log("Game Over!");
+            submitButton.disabled = true;
+        }
+
+        
+
 
 });
 
-console.log(a)
