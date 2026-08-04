4ef181de5784a56e28d9a8cb2c6b01e7405f9175
abirsaha9328-arch <abirsaha9328@gmail.com>
2026-08-04 15:31:01 +0000

    Project 4

---
+++ 07_Project/Project4/script.js
@@ -0,0 +1,16 @@
+const prevGuesses = document.querySelector(".guesses");
+const remGuesses = document.querySelector(".lastResult");
+const submitButton = document.querySelector("#subt");
+const inputValue = document.querySelector("#guessField").value
+const form = document.querySelector(".form");
+
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    var a = 12
+
+});
+
+console.log(a)
